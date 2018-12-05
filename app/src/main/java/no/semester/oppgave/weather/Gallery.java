5a0e9f790c33385e465d01a0bada9dc25c4c4036
unknown <fredrik-hulaas@hotmail.com>
2018-12-05 22:14:39 +0100

    with gallery

---
+++ app/src/main/java/no/semester/oppgave/weather/Gallery.java
@@ -0,0 +1,34 @@
+package no.semester.oppgave.weather;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class Gallery extends BaseAdapter {
+    private Context context;
+    DataBaseHelper dbHelper;
+    public Integer[] images = {
+            dbHelper.
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
