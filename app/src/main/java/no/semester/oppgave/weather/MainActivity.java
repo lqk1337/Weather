d968cf621fb3ba0c7405ef21af5f01e18ec2b3c0
unknown <fredrik-hulaas@hotmail.com>
2018-12-06 20:36:04 +0100

    minorfixes2

--- app/src/main/java/no/semester/oppgave/weather/MainActivity.java
+++ app/src/main/java/no/semester/oppgave/weather/MainActivity.java
@@ -18,6 +18,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -35,7 +36,7 @@ import no.semester.oppgave.PermissionFragment.ShowPermissions;
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
 
-    private DrawerLayout mDrawerLayout;
+    protected DrawerLayout mDrawer;
     TextView cityTextView;
     TextView descriptionTextView;
     TextView windTextView;
