d745ce18ad7e6723f4682095feeb31739d3cd1e2
unknown <fredrik-hulaas@hotmail.com>
2018-12-06 20:48:37 +0100

    with landscape

--- app/src/main/java/no/semester/oppgave/weather/MainActivity.java
+++ app/src/main/java/no/semester/oppgave/weather/MainActivity.java
@@ -116,8 +116,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-
     }
 
     public List<String> getPicturePath() {
@@ -184,8 +182,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 
-
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -206,6 +202,4 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-
-
 }
