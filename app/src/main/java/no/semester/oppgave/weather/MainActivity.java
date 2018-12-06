f45fca8013bb7b3bc3c1d7150e9f974425ac96fe
unknown <fredrik-hulaas@hotmail.com>
2018-12-06 16:52:45 +0100

    removed settings

--- app/src/main/java/no/semester/oppgave/weather/MainActivity.java
+++ app/src/main/java/no/semester/oppgave/weather/MainActivity.java
@@ -27,6 +27,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.io.File;
@@ -123,6 +124,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+
+
     }
 
     public List<String> getPicturePath() {
@@ -189,27 +192,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
--- app/src/main/java/no/semester/oppgave/weather/ShowPermissions.java
+++ app/src/main/java/no/semester/oppgave/weather/ShowPermissions.java
@@ -1,16 +1,12 @@
 package no.semester.oppgave.weather;
 
-import android.app.Activity;
 import android.os.Bundle;
 
-public class ShowPermissions extends Activity {
-
+public class ShowPermissions extends MainActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permissions);
     }
-
-
 }
